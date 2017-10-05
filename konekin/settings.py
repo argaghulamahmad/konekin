7858e6c13df7ac45901c8f8217b173eaad32eec0
Alya Putri <alyaputri99@gmail.com>
2017-10-05 15:56:47 +0700

    Added Django Apps to Installed Apps list.

--- konekin/settings.py
+++ konekin/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'add_friend',
+    'dashboard',
+    'halaman_profile',
+    'update_status'
 ]
 
 MIDDLEWARE = [
