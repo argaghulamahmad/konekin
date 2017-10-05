578e61836b8e23c3ccb6a005505f04c40ae05505
argaghulamahmad <argaghulamahmad@gmail.com>
2017-10-05 21:37:37 +0700

    Menambahkan unittest pada 'tests.py' 'dashboard' project konekin

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import resolve
+from .views import index
 
-# Create your tests here.
+
+class DashboardUnitTest(TestCase):
+    # test apakah url dashboard sudah ada
+    def test_dashboard_url_is_exist(self):
+        response = Client().get('/stats/')
+        self.assertEqual(response.status_code, 200)
+
+    # test apakah dashboard menggunakan funcsi index() pada views dashboard
+    def test_dashboard_using_index_func(self):
+        found = resolve('/stats/')
+        self.assertEqual(found.func, index)
