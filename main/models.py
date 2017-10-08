b7acda2aab24c927bc8d7c1f6b69394ea5444aeb
argaghulamahmad <argaghulamahmad@gmail.com>
2017-10-08 09:55:09 +0700

    Memperbaharui 'models.py' app 'main' project konekin

--- main/models.py
+++ main/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.forms import ImageField
-import os
 import json
 
 GENDER_CHOICES = (
@@ -8,27 +6,27 @@ GENDER_CHOICES = (
         ('F', 'Female'),
     )
 
-def get_image_path(instance, filename):
-    return os.path.join('photos', str(instance.id), filename)
-
 class User(models.Model):
     name = models.CharField(max_length=27)
     birthday = models.DateField()
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-    expertise = models.ForeignKey(UserExpertise)
+    expertise = models.ForeignKey('UserExpertise')
     description = models.TextField()
-    email = models.EmailField()
+    email = models.EmailField(unique=True, db_index=True)
 
 class UserProfile(models.Model):
-    user = models.ForeignKey(User, unique=True)
-    photo = models.ForeignKey(Photo)
+    user = models.ForeignKey('User')
+    photo = models.ImageField('profile picture', upload_to='static/media/images/avatars/',
+                                       null=True, blank=True)
 
-class Photo(models.Model):
-    profile_image = ImageField(upload_to=get_image_path, blank=True, null=True)
+    def set_avatar(self):
+        _avatar = self.avatar
+        if not _avatar:
+            self.avatar = "static/img/noprofile.svg"
 
 class UserPost(models.Model):
-    user = models.ForeignKey(User, unique=True)
-    activity = models.TextField(max_length=60)
+    user = models.ForeignKey('User')
+    post = models.TextField(max_length=60)
     date = models.DateTimeField()
 
 class UserExpertise(models.Model):
