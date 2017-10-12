c28388eebf3af55b4a287677a579244d053c4e9a
Winayaka Ruhur <winayakarsp@gmail.com>
2017-10-12 19:54:20 +0700

    mengupdate tampilan update-status

--- update_status/forms.py
+++ update_status/forms.py
@@ -6,9 +6,9 @@ class Status_Form(forms.Form):
     }
     description_attrs = {
         'type': 'text',
-        'cols': 100,
+        'cols': 45,
         'rows': 4,
         'class': 'todo-form-textarea',
         'placeholder':"what's new?"
     }
-    description = forms.CharField(label="", required=True,max_length=130, widget=forms.Textarea(attrs=description_attrs))
+    description = forms.CharField(label="", required=True, max_length=140, widget=forms.Textarea(attrs=description_attrs))
