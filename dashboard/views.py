054fa9b75def8de772f0adb19581a475eb5762e4
argaghulamahmad <argaghulamahmad@gmail.com>
2017-10-13 09:43:14 +0700

    Semifinal version of app 'dashboard'

--- dashboard/views.py
+++ dashboard/views.py
@@ -9,6 +9,6 @@ def index(request):
     response['number_of_friends'] = UserFriend.objects.count()
     response['number_of_feeds'] = UserPost.objects.count()
     response['photo_path'] = UserProfile.objects.first().photo.url[7:]
-    response['user_post'] = list(reversed(UserPost.objects.filter(user_id=id_user_main)))
+    response['user_post'] = list(reversed(UserPost.objects.filter(user_id=id_user_main)))[0]
     response['title'] = "Konekin - Stats"
     return render(request, 'dashboard.html', response)
